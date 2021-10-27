9c4d2536395fa4896a837dea469b7d8fb4490e2d
Stelk <kstel30@gmail.com>
2021-10-27 19:44:12 +0200

    Adding fonts, authentication method

--- lib/auth/main/auth.dart
+++ lib/auth/main/auth.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:levant/auth/service/authentication.dart';
+import 'package:levant/auth/main/registration.dart';
+import 'package:levant/style/mainStyle.dart';
 
 class AuthRoute extends StatefulWidget {
   @override
@@ -12,32 +13,65 @@ class _AuthRouteState extends State<AuthRoute> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: _loading
-            ? CircularProgressIndicator()
-            : Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  FlatButton(
-                      onPressed: () {
-                        setState(() => _loading = true);
-
-                        Auth().allAuthentication("Google", {}, context);
-                      },
-                      child: Text("Google sign in",
-                          style: TextStyle(color: Colors.white)),
-                      color: Colors.red),
-                  FlatButton(
-                      onPressed: () {
-                        setState(() => _loading = true);
-
-                        Auth().allAuthentication("Facebook", {}, context);
-                      },
-                      child: Text("Facebook sign in",
-                          style: TextStyle(color: Colors.white)),
-                      color: Colors.blue),
-                ],
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage(
+                "assets/background/startScreen.gif",
               ),
+              fit: BoxFit.cover),
+        ),
+        child: Center(
+          child: _loading
+              ? CircularProgressIndicator()
+              : SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.9,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Levant",
+                        style: MainFontsApp.poppins_black.copyWith(
+                            fontSize: 56,
+                            color: MainColorsApp.logo_yellow_color),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 32.0),
+                        child: TextButton(
+                          onPressed: () => Navigator.of(context).push(
+                              MaterialPageRoute(
+                                  builder: (_) => RegistrationPage())),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 3.0),
+                            child: SizedBox(
+                              width: MediaQuery.of(context).size.width,
+                              child: Text(
+                                "ENTRA",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 20),
+                              ),
+                            ),
+                          ),
+                          style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.black),
+                            overlayColor: MaterialStateProperty.all(
+                              MainColorsApp.logo_yellow_color.withOpacity(.1),
+                            ),
+                            shape: MaterialStateProperty.all(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(24),
+                                side: BorderSide(color: Colors.white),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+        ),
       ),
     );
   }
--- lib/style/mainStyle.dart
+++ lib/style/mainStyle.dart
@@ -7,6 +7,27 @@ class MainColorsApp {
   /// Color.fromRGBO(0, 0, 0, 1)
   static final Color main_color = Color.fromRGBO(0, 0, 0, 1);
 
-  /// Color.fromRGBO(194,143,245,255)
+  /// Color.fromRGBO(194, 143, 245, 255)
   static final Color primary_button_color = Color.fromRGBO(92, 137, 241, 1);
+
+  /// Color.fromRGBO(220, 248, 134, 1)
+  static final Color logo_yellow_color = Color.fromRGBO(220, 248, 134, 1);
+}
+
+class MainFontsApp {
+  /// Poppins light w300
+  static final TextStyle poppins_light =
+      TextStyle(fontFamily: 'Poppins', fontWeight: FontWeight.w300);
+
+  /// Poppins normal
+  static final TextStyle poppins_normal =
+      TextStyle(fontFamily: 'Poppins', fontStyle: FontStyle.normal);
+
+  /// Poppins bold w700
+  static final TextStyle poppins_black =
+      TextStyle(fontFamily: 'Poppins', fontWeight: FontWeight.w700);
+
+  /// Poppins bold w700
+  static final TextStyle poppins_extra_bold =
+      TextStyle(fontFamily: 'Poppins', fontWeight: FontWeight.w900);
 }
